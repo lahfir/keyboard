ccadf9e881590ff10212b6489613301e6db06fb3
Lahfir <nmhlahfir2@gmail.com>
2025-06-17 01:26:47 +0530

    Update CustomKeyboard to include additional keys and improve key uniqueness
    - Added 'A', 'V', and 'C' to the ALPHABET_WITH_SPACE array for expanded input options.
    - Modified KeyButton key assignment to use a combination of the key and a random number, enhancing key uniqueness and preventing potential rendering issues.

--- components/CustomKeyboard.tsx
+++ components/CustomKeyboard.tsx
@@ -141,7 +141,7 @@ const KeyButton: FC<{
 
 const ALPHABET_WITH_SPACE = [
     ...Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i)),
-    ' ',
+    ' ', 'A', 'V', 'C'
 ];
 
 /**
@@ -194,7 +194,7 @@ export const CustomKeyboard: FC<CustomKeyboardProps> = ({ onKeyPress }) => {
                 <>
                     {keys.map((k) => (
                         <KeyButton
-                            key={k}
+                            key={k + Math.random()}
                             letter={k}
                             width={buttonWidth}
                             height={buttonHeight}
